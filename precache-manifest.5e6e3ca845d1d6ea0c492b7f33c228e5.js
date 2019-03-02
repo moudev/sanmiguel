339b89b249287fc83d89949e440da82be71131e6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-02 18:53:31 +0000

    Deploy nodeschool/sanmiguel to github.com/nodeschool/sanmiguel.git:gh-pages

---
+++ precache-manifest.5e6e3ca845d1d6ea0c492b7f33c228e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ed3db996d85107117ad",
+    "url": "./static/css/main.f5341a53.chunk.css"
+  },
+  {
+    "revision": "1ed3db996d85107117ad",
+    "url": "./static/js/main.1ed3db99.chunk.js"
+  },
+  {
+    "revision": "29d97b95aa515819dc70",
+    "url": "./static/css/1.57dd8f01.chunk.css"
+  },
+  {
+    "revision": "29d97b95aa515819dc70",
+    "url": "./static/js/1.29d97b95.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5908cf9fa6b18eae108a89ba1dfdfd6c",
+    "url": "./static/media/logo.5908cf9f.svg"
+  },
+  {
+    "revision": "daeef266adc8db27592fe9dc1647e31b",
+    "url": "./static/media/diploma.daeef266.svg"
+  },
+  {
+    "revision": "78d9fbb4a02b93054bc4cd066cccb9b9",
+    "url": "./static/media/diploma-full.78d9fbb4.png"
+  },
+  {
+    "revision": "c15082e81693bf00df2581c56c27e35c",
+    "url": "./index.html"
+  }
+];
